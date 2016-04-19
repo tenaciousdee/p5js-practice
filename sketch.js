c3663df2da729b52c878136ab6d71bce1a75a868
Dennise Saxton <dennisepnd@gmail.com>
2016-04-19 15:09:41 -0500

    added the update method to Particle object

--- sketch.js
+++ sketch.js
@@ -9,3 +9,13 @@ Particle.prototype.run = function () {
   this.display();
   this.intersects();
 };
+
+Particle.prototype.update = function () {
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.acceleration.mult(-0.1);
+  if (this.position.x < 0) this.position.x = width;
+  if (this.position.x > width) this.position.x = 0;
+  if (this.position.y < 0) this.position.y = height;
+  if (this.position.y > height) this.position.y = 0;
+};
